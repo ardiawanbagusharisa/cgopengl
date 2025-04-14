fb01dbe8e0ad2bfd20516316884583e97c9be8e1
ardiawanbagusharisa <ardiawanbagusharisa@gmail.com>
2025-04-14 08:15:52 +0800

    Reupdate the template project

--- Template/OpenGL App 1/OpenGL App 1.cpp
+++ Template/OpenGL App 1/OpenGL App 1.cpp
@@ -1,26 +1,35 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
+void drawRectangle(float x, float y, float width, float height) {
+    glBegin(GL_QUADS);
+    glColor3f(0.0f, 1.0f, 0.0f); // Green rectangle
+    glVertex2f(x, y);
+    glVertex2f(x + width, y);
+    glVertex2f(x + width, y + height);
+    glVertex2f(x, y + height);
+    glEnd();
+}
+
 void display() {
-	glClear(GL_COLOR_BUFFER_BIT); // Clear the screen
-	glColor3f(1.0f, 0.0f, 0.0f); // Set color to red
-	glBegin(GL_QUADS); // Start drawing a square
-	glVertex2f(-0.5f, -0.5f);
-	glVertex2f(0.5f, -0.5f);
-	glVertex2f(0.5f, 0.5f);
-	glVertex2f(-0.5f, 0.5f);
-	glEnd();
-	glFlush(); // Render now
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    drawRectangle(-0.5f, -0.5f, 1.0f, 1.0f); // Draw a rectangle centered in screen
+
+    glFlush();
 }
 
 int main(int argc, char** argv) {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(400, 400);
-	glutCreateWindow("OpenGL Setup Test");
-	glewInit();
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Black background
-	glutDisplayFunc(display);
-	glutMainLoop();
-	return 0;
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("OpenGL Rectangle");
+
+    glewInit();
+    glClearColor(0, 0, 0, 1); // Black background
+
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+    return 0;
 }
